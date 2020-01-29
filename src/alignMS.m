364210dd52a87cd67aadafdc4c1441b5501ea933
jorismeurs <jorismeurs@gmail.com>
2020-01-29 08:12:27 +0000

    Update alignMS.m

--- src/alignMS.m
+++ src/alignMS.m
@@ -1,31 +1,59 @@
-% Code here
+% Functionality behind LESA_align GUI for processing .RAW files
+%
+% (c) Joris Meurs, MSc (2020)
+
 function alignMS(parameters) 
 
+% Check validity of input parameters from GUI
 validateInput(parameters);
 
+% Browse for
 [FileName, PathName] = uigetfile('.raw',...
 'MultiSelect','on');
 if isequal(FileName, 0);
    return
 end 
 fileLocation = fullfile(PathName, FileName);
-mzxmlFiles = convertRaw(fileLocation);
-for j = 1:length(mzxmlFiles)
-    peakData{j} = retrievePeaks(mzxmlFiles{j});
+
+% Retrieve peaklist per file
+try
+   mzxmlFiles = convertRaw(fileLocation);
+   for j = 1:length(mzxmlFiles)
+      peakData{j} = retrievePeaks(mzxmlFiles{j});
+   end
+catch
+   errordlg('Error during peak processing','Something went wrong with files');
+   return
 end
 
+% Generate unique peak matrix
+
+% Retrieve intensities per peak for each file
+
+% Filter variables below threshold abundance and impute remaining missing values
+
+% Export matrix to an Excel file
 
 
 end
 
+%---------------------------------
 function validateInput(params);
 
 end
-
+%---------------------------------
 function mzxmlFiles = convertRaw(rawFiles)
 
 end
-
+%---------------------------------
+function retrievePeaks(files)
+   
+end
+%---------------------------------
 function peakList = uniquePeaks(allPeaks)
+
+end
+%---------------------------------
+function intensityMatrix = generateIntensityMatrix(peakList)
 
 end
