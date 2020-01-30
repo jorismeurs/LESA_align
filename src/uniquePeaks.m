177029bb00e4e31f8f2e5425a116c66dbf4673a4
jorismeurs <jorismeurs@gmail.com>
2020-01-30 17:12:37 +0000

    Update uniquePeaks.m

--- src/uniquePeaks.m
+++ src/uniquePeaks.m
@@ -1,3 +1,23 @@
-function peakList = uniquePeaks(allPeaks)
-
+function peakList = uniquePeaks(allPeaks, tolerance)
+   peakVector = cell2mat(allPeaks);
+   r = [];
+   peakList = [];
+   for j = 1:length(allPeaks) 
+      if ~isempty(r) 
+         dup = find(r(:,1)==j)
+         if ~isempty(dup) 
+            continue
+         end
+      end
+      maxDev = ppmDeviation(peakVector(j,1),tolerance);
+      matchIons = find(peakVector(:,1) >= peakVector(j,1)-maxDev & ... 
+                        peakVector(:,1) <= peakVector(j,1)+maxDev);
+      if numel(matchIons) > 1
+          r = [r;matchIons];
+          peakList = [peakList;median(peakVector(matchIons,1))];
+      else 
+          r = [r;matchIons];
+          peakList = [peakList;peakVector(matchIons,1)];
+      end
+   end
 end
