080ea427fd3d84f1bcaf9579edad72e98c438b2e
jorismeurs <jorismeurs@gmail.com>
2020-02-17 07:04:48 +0000

    Handle empty cells

--- src/generateIntensityMatrix.m
+++ src/generateIntensityMatrix.m
@@ -1,10 +1,17 @@
 function intensityMatrix = generateIntensityMatrix(peakList,peakData,parameters)
     tolerance = parameters.tolerance;
-    intensityMatrix = zeros(length(peakData),length(peakList));
+    emptyIDX = cellfun(@(data) isempty(data),peakData,'UniformOutput',false);
+    fileCount = length(peakData)-length(emptyIDX);
+    intensityMatrix = zeros(fileCount,length(peakList));
     for j = 1:length(peakList) 
        peakMZ = peakList(j,1);
        maxDev = ppmDeviation(peakMZ,tolerance);
        for n = 1:length(peakData) 
+          if ~isempty(emptyIDX) 
+             if ~isempty(find(emptyIDX(:,1)==n))
+                continue 
+             end
+          end
           tempData = cell2mat(peakData(n,1));
           peakMatch = find(tempData(:,1) >= peakMZ-maxDev & ... 
                            tempData(:,1) <= peakMZ+maxDev);
