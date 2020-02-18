592998e3d64dd3100078c1f790179f2d074f7e69
jorismeurs <jorismeurs@gmail.com>
2020-02-18 16:35:14 +0000

    Update progress bar

---
+++ src/failedProcess.m
@@ -0,0 +1,4 @@
+function failedProcess(handles)
+
+axes(handles.axes1);
+patch([0 1 1 0],[0 0 1 1],[1 0 0]);
---
+++ src/updateProcess.m
@@ -0,0 +1,4 @@
+function updateProcess(val,handles)
+
+axes(handles.axes1);
+patch([0 val/12 val/12 0],[0 0 1 1],[0 1 0]);
