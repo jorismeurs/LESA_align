63ff9162d9c344f0c382997a0c2dd8ba0be121a9
jorismeurs <jorismeurs@gmail.com>
2024-02-25 07:49:47 +0100

    Update clusterAlign.m

--- src/clusterAlign.m
+++ src/clusterAlign.m
@@ -1,25 +1,21 @@
-function [intensityMatrix,emptyIDX] = clusterAlign(allPeaks,peakData)
-    
-    emptyIDX = find(cellfun(@isempty,peakData));
-    fileCount = length(peakData)-length(emptyIDX);
-
+function [CMZ,intensityMatrix] = clusterAlign(peakData)
+    % Function for aligning peaks using clustering based on Euclidean distance
     peakList = cell2mat(allPeaks);
     numPeaks = cellfun(@(x) length(x),allPeaks);
 
     % Clustering based on Euclidean distance
     tree = linkage(pdist(peakList(:,1)));
     clusters = cluster(tree,'CUTOFF',0.005,'CRITERION','Distance'); 
-    clusters_sorted = sort(clusters);
 
-    CRt = accumarray(clusters,prod(peakList,2))./accumarray(clusters,peakList(:,2));
+    CMZ = accumarray(clusters,prod(peakList,2))./accumarray(clusters,peakList(:,2));
 
     PR = accumarray(clusters,peakList(:,2),[],@median);
 
-    [CRt,h] = sort(CRt);
+    [CMZ,h] = sort(CMZ);
     PR = PR(h);
 
-    intensityMatrix = nan(numel(CRt),length(allPeaks));
-    for i = 1:length(allPeaks)
+    intensityMatrix = nan(numel(CMZ),length(peakData));
+    for i = 1:length(peakData)
         [j,k] = samplealign([CRt PR],allPeaks{i},'BAND',1,'WEIGHTS',[1 0]);
         intensityMatrix(j,i) = allPeaks{i}(k,2);
     end
