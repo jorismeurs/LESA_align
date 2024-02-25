3f0e6858bd454776580e6df309377d583294355c
jorismeurs <jorismeurs@gmail.com>
2024-02-25 20:57:07 +0100

    Update exportFile.m

--- src/exportFile.m
+++ src/exportFile.m
@@ -1,4 +1,4 @@
-function exportFile(mzList,intensityMatrix,j)
+function exportFile(mzList,intensityMatrix,file_names,j)
 % Function to export stored peak lists and corresponding intensities to a .csv file
     
     % Deal with export of both positive and negative mode data
@@ -6,9 +6,34 @@ function exportFile(mzList,intensityMatrix,j)
       j = 0;
     end
 
-    if j == 0
-
+    if j == 0 % Single file
+        intensityMatrix = intensityMatrix'; % Transpose matrix to have data files in rows
+        mzList = num2str(mzList');
+        T = table(file_names);
+        T.Properties.VariableNames = {'File_name'};
+        T = [T,array2table(intensityMatrix)];
+        T.Properties.VariableNames(2:end) = mzList;
+        exportName = [datestr(now,'yymmdd-HHMMSS') '.csv'];
+        writetable(T,exportName);
     else
-
+        if j == 1
+            intensityMatrix = intensityMatrix'; % Transpose matrix to have data files in rows
+            mzList = num2str(mzList');
+            T = table(file_names);
+            T.Properties.VariableNames = {'File_name'};
+            T = [T,array2table(intensityMatrix)];
+            T.Properties.VariableNames(2:end) = mzList;
+            exportName = [datestr(now,'yymmdd-HHMMSS') '_pos.csv'];
+            writetable(T,exportName);    
+        elseif j == 2
+            intensityMatrix = intensityMatrix'; % Transpose matrix to have data files in rows
+            mzList = num2str(mzList');
+            T = table(file_names);
+            T.Properties.VariableNames = {'File_name'};
+            T = [T,array2table(intensityMatrix)];
+            T.Properties.VariableNames(2:end) = mzList;
+            exportName = [datestr(now,'yymmdd-HHMMSS') '_neg.csv'];
+            writetable(T,exportName);
+        end
     end
 end
